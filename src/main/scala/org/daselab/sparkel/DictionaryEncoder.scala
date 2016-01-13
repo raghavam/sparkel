6c6d26d77989c29d647de5f80ab199da94a97894
raghavam <m.vijayaraghava@gmail.com>
2016-01-12 23:55:29 -0500

    encoded and written s axioms to file

--- src/main/scala/org/daselab/sparkel/DictionaryEncoder.scala
+++ src/main/scala/org/daselab/sparkel/DictionaryEncoder.scala
@@ -2,8 +2,9 @@ package org.daselab.sparkel
 
 import org.semanticweb.owlapi.model._
 import org.semanticweb.owlapi.apibinding.OWLManager
-import java.io.File
+import java.io.{File,PrintWriter,BufferedWriter,FileWriter}
 import collection.JavaConverters._
+import collection.mutable.{Map,Set}
 
 /**
  * Encodes the axioms in the given ontology. Each string is mapped 
@@ -14,14 +15,83 @@ import collection.JavaConverters._
  */
 object DictionaryEncoder {
   
+  private var dictionary: Map[String, Int] = Map()
+  
   def encodeAxioms(ontFilePath: String): Unit = {
     val ontology = loadOntology(ontFilePath)
+    encodeOntologyTerms(ontology)
     //convert Java set to Scala set and apply the encoding function on each axiom
-    ontology.getLogicalAxioms().asScala.foreach(encodeAndWriteToFile(_))
+    ontology.getLogicalAxioms().asScala.foreach(encodeAxiomAndWriteToFile(_))
   }
   
-  private def encodeAndWriteToFile(axiom: OWLLogicalAxiom): Unit = {
-    println(axiom.toString())
+  private def encodeOntologyTerms(ontology: OWLOntology): Unit = {
+    val ontologyConcepts = ontology.getClassesInSignature().asScala
+    val topConcept = ontology.getOWLOntologyManager().getOWLDataFactory().
+                        getOWLThing().toString();
+    var code: Int = 1
+    dictionary += (topConcept -> code)
+    code += 1
+    ontologyConcepts.foreach((concept: OWLClass) => 
+                       {dictionary += (concept.toString() -> code); code += 1})
+    val ontologyProperties = ontology.getObjectPropertiesInSignature().asScala
+    ontologyProperties.foreach((property: OWLObjectProperty) => 
+                       {dictionary += (property.toString() -> code); code += 1})
+    writeSAxiomsToFile(ontologyConcepts, topConcept)
+    writeRAxiomsToFile(ontologyProperties)
+  }
+  
+  private def writeSAxiomsToFile(ontologyConcepts: Set[OWLClass], topConcept: String): Unit = {
+    val saxiomsWriter = new PrintWriter(new BufferedWriter(
+                            new FileWriter("saxioms.txt")))
+    val topConceptCode = dictionary.get(topConcept).toString()
+    ontologyConcepts.foreach((concept: OWLClass) => {
+        val conceptCode = dictionary.get(concept.toString()).toString(); 
+        saxiomsWriter.println(conceptCode + "|" + conceptCode);
+        saxiomsWriter.println(conceptCode + "|" + topConceptCode)})
+    saxiomsWriter.close();
+  }
+  
+  private def writeRAxiomsToFile(ontologyProperties: Set[OWLObjectProperty]): Unit = {
+    
+  }
+  
+  private def encodeAxiomAndWriteToFile(axiom: OWLLogicalAxiom): Unit = {
+    axiom match {
+      case subClassAxiom: OWLSubClassOfAxiom => 
+                          handleSubClassAxiom(subClassAxiom)
+      case subObjectPropertyAxiom: OWLSubObjectPropertyOfAxiom => 
+                          handleSubObjectPropertyAxiom(subObjectPropertyAxiom)
+      case subPropertyChainAxiom: OWLSubPropertyChainOfAxiom => 
+                          handleSubPropertyChainAxiom(subPropertyChainAxiom)
+      case _ => throwException(axiom)
+    }
+  }
+  
+  private def handleSubClassAxiom(subClassAxiom: OWLSubClassOfAxiom): Unit = {
+    val subClassExpression = subClassAxiom.getSubClass()
+    val superClassExpression = subClassAxiom.getSuperClass()
+    subClassExpression match {
+      case leftAtomicConcept: OWLClass => superClassExpression match {
+          case rightAtomicConcept: OWLClass => 
+          case rightExistential: OWLObjectSomeValuesFrom => 
+          case _ => throwException(subClassAxiom)
+          }
+      case leftExistential: OWLObjectSomeValuesFrom => 
+      case objIntersection: OWLObjectIntersectionOf => 
+      case _ => throwException(subClassAxiom)
+    }
+  }
+  
+  private def handleSubObjectPropertyAxiom(subObjectPropertyAxiom: OWLSubObjectPropertyOfAxiom): Unit = {
+    println(subObjectPropertyAxiom.toString() + "  subObjProp")
+  }
+  
+  private def handleSubPropertyChainAxiom(subPropertyChainAxiom: OWLSubPropertyChainOfAxiom): Unit = {
+    println(subPropertyChainAxiom.toString() + "  subPropChain")
+  }
+  
+  private def throwException(axiom: OWLLogicalAxiom): Unit = {
+    throw new Exception("Unexpected axiom type: " + axiom.toString())
   }
   
   private def loadOntology(ontFilePath: String): OWLOntology = {
