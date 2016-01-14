11fe158a1eb05a029d6f22841c15d596aad7ae18
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-13 19:12:15 -0500

    Rule3 test1

--- src/main/scala/org/daselab/sparkel/SparkEL.scala
+++ src/main/scala/org/daselab/sparkel/SparkEL.scala
@@ -3,6 +3,7 @@ package org.daselab.sparkel
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.EmptyRDD
 import org.apache.spark.rdd._
 import main.scala.org.daselab.sparkel.Constants._
 
@@ -19,36 +20,61 @@ object SparkEL {
    */
   def initializeRDD(sc: SparkContext, dirPath: String) = {    
     // question - what is the return type for uAxioms and type1Axioms 
+    //!!!SWAP x and y here
     var uAxioms = sc.textFile(dirPath+"uAxioms.txt").map(line => {line.split("\\|") match { case Array(x,y) => (x.toInt,y.toInt)}}) 
+    var rAxioms: RDD[(Int,(Int,Int))] = sc.emptyRDD
+      
     val type1Axioms = sc.textFile(dirPath+"Type1Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y) => (x.toInt,y.toInt)}}) 
     val type2Axioms = sc.textFile(dirPath+"Type2Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y,z) => (x.toInt,(y.toInt,z.toInt))}})
+    val type3Axioms = sc.textFile(dirPath+"Type3Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y,z) => (x.toInt,(y.toInt,z.toInt))}})
+    val type4Axioms = sc.textFile(dirPath+"Type3Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y,z) => (x.toInt,(y.toInt,z.toInt))}})
+    val type5Axioms = sc.textFile(dirPath+"Type1Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y) => (x.toInt,y.toInt)}})
+    val type6Axioms = sc.textFile(dirPath+"Type3Axioms.txt").map(line => {line.split("\\|") match { case Array(x,y,z) => (x.toInt,(y.toInt,z.toInt))}})
+    
     
     //persist the RDDs
     type1Axioms.persist()
     type2Axioms.persist()
+    type3Axioms.persist()
+    type4Axioms.persist()
+    type5Axioms.persist()
+    type6Axioms.persist()
     
     //return the uAxioms and 
-     (uAxioms,type1Axioms,type2Axioms)   
+     (uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms)   
   }
   
   //completion rule1
-  def completionRule1(uAxioms: RDD[(Int,Int)], type1Axioms: RDD[(Int,Int)]) = {
+  def completionRule1(uAxioms: RDD[(Int,Int)], type1Axioms: RDD[(Int,Int)]): RDD[(Int,Int)] = {
     val r1Join = type1Axioms.join(uAxioms).map( { case (k,v) => v})
     val uAxiomsNew = uAxioms.union(r1Join).distinct // uAxioms is immutable as it is input parameter
     uAxiomsNew    
   }
   
   //completion rule 2
-  def completionRule2(uAxioms: RDD[(Int,Int)], type2Axioms: RDD[(Int,(Int,Int))]) = {
+  def completionRule2(uAxioms: RDD[(Int,Int)], type2Axioms: RDD[(Int,(Int,Int))]): RDD[(Int,Int)] = {
   
     val r2Join1 = type2Axioms.join(uAxioms)
     val r2Join1Remapped = r2Join1.map({ case (k,((v1,v2),v3)) => (v1,(v2,v3))})
+    r2Join1Remapped.foreach(println(_))
     val r2Join2 = r2Join1Remapped.join(uAxioms)
+    r2Join2.foreach(println(_))
     val r2JoinOutput = r2Join2.filter({case (k,((v1,v2),v3)) => v2 == v3}).map( {case (k,((v1,v2),v3)) => (v1,v2)})
     val uAxiomsNew = uAxioms.union(r2JoinOutput).distinct // uAxioms is immutable as it is input parameter
     uAxiomsNew 
     
   }
+   
+  def completionRule3(uAxioms: RDD[(Int,Int)], rAxioms: RDD[(Int,(Int,Int))], type3Axioms: RDD[(Int,(Int,Int))]): RDD[(Int,(Int,Int))] = {
+    
+    val r3Join = type3Axioms.join(uAxioms)
+    val r3Output = r3Join.map({case (k,((v1,v2),v3)) => (v1,(v3,v2))})
+    val rAxiomsNew = rAxioms.union(r3Output).distinct
+    rAxiomsNew
+    
+  }
+  
+  
   
   /*
    * The main method that inititalizes and calls each function corresponding to the completion rule 
@@ -61,10 +87,11 @@ object SparkEL {
     else {   
       val conf = new SparkConf().setAppName("SparkEL")
       val sc = new SparkContext(conf)
-      var(uAxioms,type1Axioms,type2Axioms) = initializeRDD(sc, args(0))
-      println("Before: uAxioms count is- "+ uAxioms.count);
-      uAxioms = completionRule2(uAxioms,type2Axioms);
-      println("After: uAxioms count is- "+ uAxioms.count);
+      var(uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms) = initializeRDD(sc, args(0))
+      println("Before: uAxioms count is- "+ rAxioms.count);
+      //uAxioms = completionRule2(uAxioms,type2Axioms);
+      rAxioms = completionRule3(uAxioms,rAxioms,type3Axioms);
+      println("After: uAxioms count is- "+ rAxioms.count);
       
     }
   }
