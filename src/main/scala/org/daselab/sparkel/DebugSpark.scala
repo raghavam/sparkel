94965a5c09b020f28053a926f4a6a6b81a7eebfd
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-20 10:00:46 -0500

    return type of initRDD from val to var. Checkpoitning should fail.

--- src/main/scala/org/daselab/sparkel/DebugSpark.scala
+++ src/main/scala/org/daselab/sparkel/DebugSpark.scala
@@ -168,7 +168,7 @@ object DebugSpark {
       val sc = new SparkContext(conf)
       sc.setCheckpointDir(CheckPointDir) //set checkpoint directory. See directions here: https://jaceklaskowski.gitbooks.io/mastering-apache-spark/content/spark-rdd-checkpointing.html
       
-      val(uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms) = initializeRDD(sc, args(0))
+      var(uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms) = initializeRDD(sc, args(0))
      
       //compute closure
       var prevUAxiomsCount: Long = 0
