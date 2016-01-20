60a17d73bf0fb1697b99e57b78b28eb37bd8614b
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-19 19:25:16 -0500

    debug

--- src/main/scala/org/daselab/sparkel/DebugSpark.scala
+++ src/main/scala/org/daselab/sparkel/DebugSpark.scala
@@ -170,7 +170,7 @@ object DebugSpark {
       println("Before closure computation. Initial #uAxioms: "+ currUAxiomsCount+", Initial #rAxioms: "+ currRAxiomsCount)
       var counter=0;
       
-      while(counter==10){
+      while(counter==5){
        
         
         //debugging 
