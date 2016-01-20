4934a58a40074c4564cfc37e7c05a1f14bf9d7af
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-20 03:17:08 -0500

    SparEL checkpoint test 2

--- src/main/scala/org/daselab/sparkel/SparkEL.scala
+++ src/main/scala/org/daselab/sparkel/SparkEL.scala
@@ -157,9 +157,9 @@ object SparkEL {
       
       val conf = new SparkConf().setAppName("SparkEL")
       val sc = new SparkContext(conf)
-      //sc.setCheckpointDir(CheckPointDir) //set checkpoint directory. See directions here: https://jaceklaskowski.gitbooks.io/mastering-apache-spark/content/spark-rdd-checkpointing.html
+      sc.setCheckpointDir(CheckPointDir) //set checkpoint directory. See directions here: https://jaceklaskowski.gitbooks.io/mastering-apache-spark/content/spark-rdd-checkpointing.html
       
-      val(uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms) = initializeRDD(sc, args(0))
+      var(uAxioms,rAxioms, type1Axioms,type2Axioms,type3Axioms,type4Axioms,type5Axioms,type6Axioms) = initializeRDD(sc, args(0))
      
       //compute closure
       var prevUAxiomsCount: Long = 0
