b2fea30cad7b06b9c163605f86fab5b7a2b2a88a
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-21 18:29:14 -0500

    debug - removed function from debug spark, all in main

--- src/main/scala/org/daselab/sparkel/DebugSpark.scala
+++ src/main/scala/org/daselab/sparkel/DebugSpark.scala
@@ -83,7 +83,8 @@ object DebugSpark {
         val t_beginLoop = System.nanoTime()
         
         //uAxioms = completionRule1(uAxioms, type1Axioms) //Rule1
-        val r1Join = type1Axioms.join(uAxioms).map( { case (k,v) => v})
+        val r1Join = type1Axioms.join(uAxioms).map( { case (k,v) => v}).cache()
+        r1Join.count()
         uAxioms = uAxioms.union(r1Join).distinct        
         uAxioms.cache()  
         println("uAxioms count: "+uAxioms.count())
