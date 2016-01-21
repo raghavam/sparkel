a3d3e667b0dfe31a9a8d99ee26f75a0165bde036
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-01-21 16:52:20 -0500

    Testing iterations using a sample TestIterations.scala

---
+++ src/main/scala/org/daselab/sparkel/TestIterations.scala
@@ -0,0 +1,35 @@
+package main.scala.org.daselab.sparkel
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd._
+
+object TestIterations {
+  
+  def main(args: Array[String]): Unit = {
+    
+    val conf = new SparkConf().setAppName("SparkEL")
+    val sc = new SparkContext(conf)
+    var rdd = sc.parallelize(List(1, 2, 3, 4))
+    var i = 0
+    
+    while(i < 3) {
+      val t_beginLoop = System.nanoTime()
+      
+      val result = rdd.flatMap{x => Thread.sleep(1000); List(x)}
+      rdd = result.map(x => x * 10)
+      println("Count: " + rdd.count)
+      i += 1
+      
+      val t_endLoop = System.nanoTime()
+      println("Loop "+ i +": Time taken: "+ (t_endLoop - t_beginLoop)/1e6 +" ms.")
+      
+      
+   }//while loop closes
+    
+    
+    
+  }
+  
+}
