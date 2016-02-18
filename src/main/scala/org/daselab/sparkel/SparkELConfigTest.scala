68f7ff681daf0f82ec62e498d2ce48d73545d20d
raghavam <m.vijayaraghava@gmail.com>
2016-02-17 23:07:30 -0500

    removed call to require() in initializeRDD()

--- src/main/scala/org/daselab/sparkel/SparkELConfigTest.scala
+++ src/main/scala/org/daselab/sparkel/SparkELConfigTest.scala
@@ -25,7 +25,8 @@ object SparkELConfigTest {
    * Initializes all the RDDs corresponding to each axiom-type. 
    */
   def initializeRDD(sc: SparkContext, dirPath: String) = {
-    require(numPartitions != -1, "set numPartitions before calling this method")
+//    require(numPartitions != -1, "set numPartitions before calling this method")
+    // set numPartitions before calling this method
     val hashPartitioner = new HashPartitioner(numPartitions)
     val uAxioms = sc.textFile(dirPath + "sAxioms.txt").map[(Int, Int)](line => { 
       line.split("\\|") match { case Array(x, y) => (y.toInt, x.toInt) } })
