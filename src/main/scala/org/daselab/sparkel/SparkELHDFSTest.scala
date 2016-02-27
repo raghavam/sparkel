89300673d673c4c87f8f0980975d0c0c83bf4956
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-02-26 20:24:42 -0500

    debug rule2

--- src/main/scala/org/daselab/sparkel/SparkELHDFSTest.scala
+++ src/main/scala/org/daselab/sparkel/SparkELHDFSTest.scala
@@ -103,6 +103,36 @@ object SparkELHDFSTest {
 
   }
 
+   def completionRule2_new(uAxioms: RDD[(Int, Int)], type2Axioms: RDD[(Int, (Int, Int))]): RDD[(Int, Int)] = {
+
+    var t_begin = System.nanoTime()
+    val r2Join1 = type2Axioms.join(uAxioms, numPartitions)
+    val r2Join1_count = r2Join1.persist(StorageLevel.MEMORY_ONLY_SER).count
+    var t_end = System.nanoTime()
+    println("r2Join1: type2Axioms.join(uAxioms). Count= " +r2Join1_count+", Time taken: "+(t_end - t_begin) / 1e6 + " ms")
+    
+    t_begin = System.nanoTime()
+    val r2Join1Remapped = r2Join1.map({ case (k, ((v1, v2), v3)) => (v1, (v2, v3)) })
+    val r2Join2 = r2Join1Remapped.join(uAxioms, numPartitions)
+    val r2Join2_count = r2Join2.cache().count
+    t_end = System.nanoTime()
+    println("r2Join2: r2Join1.map().join(uAxioms). Count= " +r2Join2_count+", Time taken: "+(t_end - t_begin) / 1e6 + " ms")
+    
+    
+    val r2JoinOutput = r2Join2.filter({ case (k, ((v1, v2), v3)) => v2 == v3 }).map({ case (k, ((v1, v2), v3)) => (v1, v2) })
+    // uAxioms is immutable as it is input parameter
+    
+    t_begin = System.nanoTime()
+    val uAxiomsNew = uAxioms.union(r2JoinOutput).distinct.partitionBy(type2Axioms.partitioner.get)
+    val uAxiomsNew_count = uAxiomsNew.persist(StorageLevel.MEMORY_ONLY_SER).count
+    t_end = System.nanoTime()
+    println("uAxiomsNew: uAxioms.union(r2Join.filter()). Count= " +uAxiomsNew_count+", Time taken: "+(t_end - t_begin) / 1e6 + " ms")
+    
+    uAxiomsNew
+
+  }
+  
+  
   //completion rule 3
   def completionRule3(uAxioms: RDD[(Int, Int)], rAxioms: RDD[(Int, (Int, Int))], type3Axioms: RDD[(Int, (Int, Int))]): RDD[(Int, (Int, Int))] = {
 
