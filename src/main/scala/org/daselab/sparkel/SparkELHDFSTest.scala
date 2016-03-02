3489749b25a44768195ef9401aaf6eaf70ed74ec
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-03-01 22:12:30 -0500

    debug rule2

--- src/main/scala/org/daselab/sparkel/SparkELHDFSTest.scala
+++ src/main/scala/org/daselab/sparkel/SparkELHDFSTest.scala
@@ -457,7 +457,7 @@ object SparkELHDFSTest {
     val type2FillersA2 = type2Collect.map({ case (a1,(a2,b)) => a2}).toSet
     val type2FillersA1A2= type2FillersA1.union(type2FillersA2)    
     
-    println("Count of elements in type2FillersA1: "+type2FillersA1.size+"type2FillersA2"+type2FillersA2.size+"type2FillersA1A2"+type2FillersA1A2.size)
+    println("Count of elements in type2FillersA1: "+type2FillersA1.size+" \n type2FillersA2: "+type2FillersA2.size+" \n type2FillersA1A2: "+type2FillersA1A2.size)
     
     val type2FillersBroadcast = sc.broadcast(type2FillersA1A2)   
     
