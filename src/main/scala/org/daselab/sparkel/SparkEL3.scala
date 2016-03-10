f01a7856ee0b406a7a3b54a6ba7af51fe508f293
raghavam <m.vijayaraghava@gmail.com>
2016-03-10 08:47:14 -0500

    registered StructField

--- src/main/scala/org/daselab/sparkel/SparkEL3.scala
+++ src/main/scala/org/daselab/sparkel/SparkEL3.scala
@@ -126,7 +126,8 @@ object SparkEL3 {
     val t_init = System.nanoTime()
 
     val conf = new SparkConf().setAppName("SparkEL3")
-    conf.registerKryoClasses(Array(classOf[org.apache.spark.sql.types.StructType]))
+    conf.registerKryoClasses(Array(classOf[org.apache.spark.sql.types.StructType], 
+        classOf[org.apache.spark.sql.types.StructField]))
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
     val (sAxioms, rAxioms, type1Axioms, type2Axioms, type3Axioms, type4Axioms, 
