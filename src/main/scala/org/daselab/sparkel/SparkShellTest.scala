18c90ea623f2825ffe6be51ae8e460c6e03bda88
raghavam <m.vijayaraghava@gmail.com>
2016-04-24 14:12:25 -0400

    removing persist

--- src/main/scala/org/daselab/sparkel/SparkShellTest.scala
+++ src/main/scala/org/daselab/sparkel/SparkShellTest.scala
@@ -235,8 +235,8 @@ var numPartitions = -1 // later initialized from command line
       //Rule 1
       var t_begin_rule = System.nanoTime()
       var currDeltaURule1 = completionRule1(uAxiomsFinal, type1Axioms,loopCounter)
-      currDeltaURule1 = currDeltaURule1.setName("deltaURule1_"+loopCounter)
-                                       .persist(StorageLevel.MEMORY_AND_DISK)
+//      currDeltaURule1 = currDeltaURule1.setName("deltaURule1_"+loopCounter)
+//                                       .persist(StorageLevel.MEMORY_AND_DISK)
 //                                       .count() // to force persist()
       var t_end_rule = System.nanoTime()
       println("----Completed rule1---- : ")
