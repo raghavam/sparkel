1ce611eb94092cf7222876e2a933eff345ae0be5
Sambhawa Priya <sambhawa.priya@gmail.com>
2016-05-03 19:33:10 -0400

    Removing isEmpty() calls from rule6

--- src/main/scala/org/daselab/sparkel/SparkShellTest.scala
+++ src/main/scala/org/daselab/sparkel/SparkShellTest.scala
@@ -279,8 +279,8 @@ object SparkShellTest {
                              .map({ case (r1, ((r2, r3), (x, y))) => ((r2, y), (r3, x)) })
                              .partitionBy(hashPartitioner)  
                              
-    if(r6Join11.isEmpty())
-        return sc.emptyRDD
+//    if(r6Join11.isEmpty())
+//        return sc.emptyRDD
     
     val rAxiomsFilterOnR2 = rAxioms.filter{case (r2, (y, z)) => type6R2.contains(r2)}
                                      .map({ case (r2, (y, z)) => ((r2, y), z)}) //for r6Join2
@@ -314,8 +314,8 @@ object SparkShellTest {
                              .map({ case (r2,((r1,r3), (x, y))) => ((r1, y), (r3, x)) })
                              .partitionBy(hashPartitioner)
 
-   if(r6Join21.isEmpty())
-       return sc.emptyRDD
+//   if(r6Join21.isEmpty())
+//       return sc.emptyRDD
                              
     
    val rAxiomsFilterOnR1 = rAxioms.filter{case (r1, (y, z)) => type6R2.contains(r1)}
@@ -692,14 +692,18 @@ object SparkShellTest {
        
        var rAxiomsRule6: RDD[(Int, (Int, Int))] = sc.emptyRDD
        
-       if(currDeltaRRule6.isEmpty()){
-         rAxiomsRule6 = rAxiomsRule5
-       }
-       else{
-        rAxiomsRule6 = rAxiomsRule5.union(currDeltaRRule6)
+//       if(currDeltaRRule6.isEmpty()){
+//         rAxiomsRule6 = rAxiomsRule5
+//       }
+//       else{
+//        rAxiomsRule6 = rAxiomsRule5.union(currDeltaRRule6)
+//                                   .partitionBy(hashPartitioner)
+//        rAxiomsRule6 = customizedDistinctForRAxioms(rAxiomsRule6).setName("rAxiomsRule6_"+loopCounter)
+//       }
+       
+      rAxiomsRule6 = rAxiomsRule5.union(currDeltaRRule6)
                                    .partitionBy(hashPartitioner)
-        rAxiomsRule6 = customizedDistinctForRAxioms(rAxiomsRule6).setName("rAxiomsRule6_"+loopCounter)
-       }
+      rAxiomsRule6 = customizedDistinctForRAxioms(rAxiomsRule6).setName("rAxiomsRule6_"+loopCounter)
       
       uAxiomsFinal = uAxiomsRule2
       rAxiomsFinal = rAxiomsRule6
